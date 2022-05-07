1711b44319b941677c4c51f4d2f85196e032dbd7
Pranay Kothari <pranay.kothari109@gmail.com>
2022-05-07 16:27:54 +0530

    added env

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-const express = require('express');
-const logRouter = require('./router/log');
-const cors = require('cors');
+const express = require("express");
+const logRouter = require("./router/log");
+const cors = require("cors");
+const dotenv = require("dotenv");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+dotenv.config();
+
+const PORT = process.env.PORT || 3001;
 
 // router
-app.use('/', logRouter);
+app.use("/", logRouter);
+app.get("/", (req, res) => {
+  res.send("nodejs filesystem");
+});
 
-app.listen(3001);
+app.listen(PORT);
