77ea9a275c30e618aed3a54243e5ca0160efa7b8
Pranay Kothari <pranay.kothari109@gmail.com>
2022-05-08 19:16:57 +0530

    moved all date and related variables inside createLog module

--- module/createLog.js
+++ module/createLog.js
@@ -1,34 +1,33 @@
-const fs = require('fs');
-
-const currentDate = new Date();
-
-//getting date
-const date = `${currentDate.getFullYear()}-${currentDate.getMonth()+1}-${currentDate.getDate()}`;
-
-// getting time
-const time = `${currentDate.getHours()}-${currentDate.getMinutes()}-${currentDate.getSeconds()}`
-const fileTitle = date+ " "+time;
-
-//content of file as timestamp
-const content = currentDate.toTimeString();
-
-// writing into file
-const dir = './log';
-if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir, {
-        recursive: true
-    });
-}
+const fs = require("fs");
 
 module.exports.createLog = (req, res, next) => {
-    fs.writeFile( `${dir}/${fileTitle}.txt`, content, (err) => {
-        if(err) { 
-            return console.log(err);
-        }
-        else{
-            res.send("successfully created")
-            console.log(fileTitle);
-        }
-    })
+  const currentDate = new Date();
 
-}
+  //getting date
+  const date = `${currentDate.getFullYear()}-${
+    currentDate.getMonth() + 1
+  }-${currentDate.getDate()}`;
+
+  // getting time
+  const time = `${currentDate.getHours()}-${currentDate.getMinutes()}-${currentDate.getSeconds()}`;
+  const fileTitle = date + " " + time;
+
+  //content of file as timestamp
+  const content = currentDate.toTimeString();
+
+  // writing into file
+  const dir = "./log";
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, {
+      recursive: true,
+    });
+  }
+  fs.writeFile(`${dir}/${fileTitle}.txt`, content, (err) => {
+    if (err) {
+      return console.log(err);
+    } else {
+      res.send("successfully created");
+      console.log(fileTitle);
+    }
+  });
+};
